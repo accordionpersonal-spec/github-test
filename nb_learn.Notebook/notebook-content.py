a11abff7ab9d22c8834b8dc7f09cea82d02c1788
accordionpersonal-spec <accordionpersonal@gmail.com>
2026-08-20 16:20:04 +0530

    Committing 2 items from workspace 46e6180e-3ee4-4787-8055-fb88fbf92ea2

---
+++ nb_learn.Notebook/notebook-content.py
@@ -0,0 +1,34 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "066f63f7-174d-4545-8e20-0bfa8c879224",
+# META       "default_lakehouse_name": "lh_learn",
+# META       "default_lakehouse_workspace_id": "46e6180e-3ee4-4787-8055-fb88fbf92ea2",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "066f63f7-174d-4545-8e20-0bfa8c879224"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+# Welcome to your new notebook
+# Type here in the cell editor to add code!
+print("Hello")
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
