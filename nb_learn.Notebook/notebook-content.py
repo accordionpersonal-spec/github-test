9dc2e6a888b3e5baaed686d2b1cbdf09a2190fe6
accordionpersonal-spec <accordionpersonal@gmail.com>
2026-08-20 22:08:43 +0530

    Committing 1 item from workspace 46e6180e-3ee4-4787-8055-fb88fbf92ea2

--- nb_learn.Notebook/notebook-content.py
+++ nb_learn.Notebook/notebook-content.py
@@ -8,12 +8,13 @@
 # META   },
 # META   "dependencies": {
 # META     "lakehouse": {
-# META       "default_lakehouse": "066f63f7-174d-4545-8e20-0bfa8c879224",
+# META       "default_lakehouse": "8c879224-0bfa-8e20-4545-174d066f63f7",
 # META       "default_lakehouse_name": "lh_learn",
-# META       "default_lakehouse_workspace_id": "46e6180e-3ee4-4787-8055-fb88fbf92ea2",
+# META       "default_lakehouse_workspace_id": "00000000-0000-0000-0000-000000000000",
 # META       "known_lakehouses": [
 # META         {
-# META           "id": "066f63f7-174d-4545-8e20-0bfa8c879224"
+# META           "id": "8c879224-0bfa-8e20-4545-174d066f63f7",
+# META           "workspace_id": "00000000-0000-0000-0000-000000000000"
 # META         }
 # META       ]
 # META     }
@@ -24,7 +25,7 @@
 
 # Welcome to your new notebook
 # Type here in the cell editor to add code!
-print("Hello")
+print("Hello") 
 
 # METADATA ********************
 
